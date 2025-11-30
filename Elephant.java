4c0d74869556ea0befd36f44ed7d931cc4110014
Minigopal <archer.arjunraj@gmail.com>
2025-11-30 17:16:41 -0500

    completed part 2
    - added a new sprite for the elephant
        - set up idle animation for elephant
    - added new bg for MyWorld
    - added new sprite for the apple

--- Elephant.java
+++ Elephant.java
@@ -13,30 +13,77 @@ public class Elephant extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     String facing = "right";
+    GreenfootSound eatApple = new GreenfootSound("eated_apple.wav");
+    
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    
+    SimpleTimer animationTimer = new SimpleTimer();
+    public Elephant()
+        {
+            for (int i = 0; i < idleRight.length; i++)
+            {
+                idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+                idleRight[i].scale(75, 75);
+            }
+            
+            for (int i = 0; i < idleLeft.length; i++)
+            {
+                idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+                idleLeft[i].mirrorHorizontally();
+                idleLeft[i].scale(75, 75);
+            }
+            
+            animationTimer.mark();
+        }
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if (animationTimer.millisElapsed() < 200)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+        }else
+        {
+            setImage(idleLeft[imageIndex]);
+        }
+        
+        
+        imageIndex = (imageIndex + 1) % idleRight.length;
+    }
+    
+    
     public void act()
     {
         // Add your action code here.
         int dx = 4;
         int dy = getY() + 2;
         int x = getX();
-        if(Greenfoot.isKeyDown("left"))
+        
+        
+        
+        if(Greenfoot.isKeyDown("left") || (Greenfoot.isKeyDown("a")))
         {
             dx = -dx;
             move(dx);
             facing = "left";
         }
-        if(Greenfoot.isKeyDown("right"))
+        if(Greenfoot.isKeyDown("right") || (Greenfoot.isKeyDown("d")))
         {
             dx = dx;
             move(dx);
             facing = "right";
         }
         
-        if(Greenfoot.isKeyDown("up"))
-        {
-            setLocation(0, dy);
-        }
+        
+        
+        
         eat();
+        animateElephant();
     }
     
     public void eat()
@@ -47,6 +94,8 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.spawnApple();
             world.increaseScore();
+            
+            eatApple.play();
         }
     }
 }
